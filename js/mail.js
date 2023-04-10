eec330808dae24942c8474dab7cb1441a17ea2e1
Courdevelops <courdevelops@gmail.com>
2023-04-10 17:16:40 -0400

    email engine added

--- js/mail.js
+++ js/mail.js
@@ -17,4 +17,25 @@ contactForm.addEventListener('submit', (e) => {
         subject: subject.value,
         message: message.value
     };
+
+    // console.log(formData);
+
+    // HTTP Request methods
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success'){
+            alert('Email sent');
+            name.value = '';
+            email.value = '';
+            subject.value = '';
+            message.value = '';
+        } else {
+            alert('Something went wrong!')
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
 });
